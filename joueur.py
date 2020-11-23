03390d59d25afd5f228358ad2fa3884d01ea2289
LeaDevaux <72825759+LeaDevaux@users.noreply.github.com>
2020-11-23 23:02:25 +0100

    Update joueur.py
    ok avec image

--- joueur.py
+++ joueur.py
@@ -1,17 +1,19 @@
 import pygame
 
 class Joueur (pygame.sprite.Sprite):
-    def __init__ (self,x,y,taille):
+    def __init__ (self,x,y,image):
 
         super().__init__()
         self.x=x
         self.y=y
-        self.taille=taille
-        self.rect=pygame.Rect(self.x,self.y,self.taille[0],self.taille[1])
+        self.image=image
+        self.rect=self.image.get_rect()
+        #self.rect=pygame.Rect(self.x,self.y,self.taille[0],self.taille[1])
 
     def afficher(self,surface):
-        pygame.draw.rect(surface,(255,0,0),self.rect)
-
+        #pygame.draw.rect(surface,(255,0,0),self.rect)
+        surface.blit(self.image,self.rect)
+        
     def mouvement ( self, vitesse):
         self.rect.x+=vitesse
         

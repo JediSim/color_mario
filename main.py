86e19077e507647dab596aeff47d386fd66d2e8a
Juliette-Neyrat <72942347+Juliette-Neyrat@users.noreply.github.com>
2020-11-23 19:15:54 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,29 +2,39 @@ import pygame
 import sys
 from ennemi import Ennemi
 from joueur import Joueur
+import time
+
 
 class Jeu:
     def __init__(self):
+        successes, failures = pygame.init()
+        print("{0} successes and {1} failures".format(successes, failures))
         self.ecran = pygame.display.set_mode((1100, 600))
         pygame.display.set_caption('Color mario')
         self.jeu_encours = True
+        self.taille = [32,64]
         self.ennemi_x = 600
         self.ennemi_y = 200
-        self.taille = [32,64]
+        self.ennemi_vitesse_y = 10 #permet de faire descendre l'ennemi
         self.ennemi = Ennemi(self.ennemi_x,self.ennemi_y,self.taille)
         #joueur
         self.joueur_x = 600
         self.joueur_y = 200
         self.joueur_vitesse_x=0
-        self.taille = [32,64]
         self.joueur = Joueur(self.joueur_x,self.joueur_y,self.taille)
 
     def boucle_principale(self):
         # boucle principale du jeu
+        clock = pygame.time.Clock()
+        FPS = 60 #frame par sec
+
 
         while self.jeu_encours:
+            clock.tick(FPS)
 
             for event in pygame.event.get():
+                self.ennemi_vitesse_y = -1
+                
                 if event.type == pygame.QUIT:
                     sys.exit()
 
@@ -38,6 +48,7 @@ class Jeu:
                         self.joueur_vitesse_x= 0                # touche relachée 
                     if event.key==pygame.K_LEFT:
                         self.joueur_vitesse_x= 0
+                
                         
 
             self.joueur.mouvement(self.joueur_vitesse_x)
@@ -51,5 +62,3 @@ if __name__ == '__main__':
     pygame.init()
     Jeu().boucle_principale()
     pygame.quit()
-
-

c395d2f18aa4de868bcfc5d25f1a2555bf6a5210
Juliette-Neyrat <72942347+Juliette-Neyrat@users.noreply.github.com>
2020-11-30 11:11:04 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from sol import Sol
 from ennemi import Ennemi
 from joueur import Joueur
 import time
@@ -13,10 +14,14 @@ class Jeu:
         pygame.display.set_caption('Color mario')
         self.jeu_encours = True
         self.taille = [32,64]
-        self.ennemi_x = 600
-        self.ennemi_y = 200
-        self.ennemi_vitesse_y = 10 #permet de faire descendre l'ennemi
+        
+        #ennemi
+        self.ennemi_x = 550
+        self.ennemi_y = 0
         self.ennemi = Ennemi(self.ennemi_x,self.ennemi_y,self.taille)
+        self.gravite = (0,4) #pour impression joueur tombe
+        self.resistance = (0,0)
+        
         #joueur
         self.joueur_x = 550
         self.joueur_y = 550
@@ -24,7 +29,8 @@ class Jeu:
         self.image = pygame.transform.scale(imgjoueur,(30,30))
         self.joueur = Joueur(self.joueur_x,self.joueur_y,self.image)
         
-        
+        #Sol
+        self.sol=Sol()
 
     def boucle_principale(self):
         # boucle principale du jeu
@@ -36,7 +42,6 @@ class Jeu:
             clock.tick(FPS)
 
             for event in pygame.event.get():
-                self.ennemi_vitesse_y = -1
                 
                 if event.type == pygame.QUIT:
                     self.jeu_encours=False
@@ -51,15 +56,23 @@ class Jeu:
                         self.joueur_vitesse_x= 0                # touche relachée 
                     if event.key==pygame.K_LEFT:
                         self.joueur_vitesse_x= 0
+            if self.sol.rect.colliderect(self.ennemi.rect):
+                self.ennemi.rect.y = 0
+
                 
                         
 
             self.joueur.mouvement(self.joueur_vitesse_x)
             self.ecran.fill((255,255,255))
             self.ennemi.afficher(self.ecran)
-            self.joueur.afficher(self.ecran)
+            self.sol.afficher(self.ecran)
+            self.gravite_jeu()
+            #self.joueur.afficher(self.ecran)
             
             pygame.display.flip()
+            
+    def gravite_jeu(self):
+        self.ennemi.rect.y += self.gravite[1] + self.resistance[1]
 
 if __name__ == '__main__':
     pygame.init()

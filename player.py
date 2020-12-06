b8dd6085370ff1b563e867eb674378cdd559f119
LeaDevaux <72825759+LeaDevaux@users.noreply.github.com>
2020-12-06 23:04:01 +0100

    Update player.py

--- player.py
+++ player.py
@@ -5,11 +5,16 @@ class Player:
         self.x = 240
         self.y = 400
         self.image = pygame.transform.scale(pygame.image.load('images/player.png'),(30,30))
+        self.rect=self.image.get_rect()
         self.ecran = ecran
         
 
     def mouvement ( self, vitesse):
-        self.x+=vitesse 
+        self.x+=vitesse
+        self.rect.x=self.x
+        self.rect.y=self.y
 
     def affiche(self):
-        self.ecran.blit(self.image,(self.x,self.y))
+       self.rect.x=self.x
+        self.rect.y=self.y
+        surface.blit(self.image,self.rect)

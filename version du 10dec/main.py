96c6a1f769001467809cbb8b0b7c70b5e710ec24
LeaDevaux <72825759+LeaDevaux@users.noreply.github.com>
2020-12-10 21:35:26 +0100

    Add files via upload

---
+++ version du 10dec/main.py
@@ -0,0 +1,17 @@
+import pygame
+from menu import*
+ 
+pygame.init()
+pygame.font.init()
+ 
+RUNNING = True
+ 
+ecran = pygame.display.set_mode((500,500)) # ecran de dimenssion 500,500
+ 
+myfont = pygame.font.SysFont('Helvetic', 20)
+
+if __name__ == "__main__":
+    
+    menu = Menu(ecran)
+    menu.run()
+

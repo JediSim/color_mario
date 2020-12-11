fc6bf5ea842ce63c7b58c82afb8f37aa2cfbc7ca
JediSim <72975805+JediSim@users.noreply.github.com>
2020-12-11 10:07:29 +0100

    Delete sol.py

--- color_mario-menu-joueur-ennemi-sans-collision/sol.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Sol(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.rect = pygame.Rect(0,600,1200,1)
-
-    def afficher(self, surface):
-        pygame.draw.rect(surface, (0,255,0), self.rect)

ad1b4b6859492c0479056a6d9d876972c057c869
JediSim <72975805+JediSim@users.noreply.github.com>
2020-12-11 09:44:34 +0100

    score + position aléatoire de l'ennemi

--- version du 10dec/jeu.py
+++ version du 10dec/jeu.py
@@ -4,6 +4,8 @@ from menu import*
 from player import*
 from ennemi import*
 from sol import*
+from random import*
+from score import*
 
 class Jeu:
     def __init__(self,ecran,menu):
@@ -12,6 +14,7 @@ class Jeu:
         self.FPS = 30
         self.clock = pygame.time.Clock()
         self.menu = menu
+        self.score = Score(ecran)
 
         #----------joueur------------
         self.joueur_vitesse_x = 0
@@ -19,7 +22,7 @@ class Jeu:
 
         #----------ennemi------------
         self.ennemi_vitesse_y = 2
-        self.ennemi = Ennemi(250,0,[32,64])
+        self.ennemi = Ennemi(randint(50,450),0,[32,64])
         self.gravite = (0,4) #pour impression joueur tombe
         self.resistance = (0,0)
 
@@ -37,36 +40,33 @@ class Jeu:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: # pouvoir quitter la ecran 
                     RUNNING = False
-
-                elif event.type == pygame.KEYDOWN: # touches presse 
+                    quitter = True
+                elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
                         self.joueur_vitesse_x= -5
                     elif event.key == pygame.K_RIGHT:
                         self.joueur_vitesse_x= 5
-                    
-                elif event.type==pygame.KEYUP:  # touche relachée 
+                elif event.type==pygame.KEYUP:
                     if event.key == pygame.K_RIGHT:
-                        self.joueur_vitesse_x= 0               
+                        self.joueur_vitesse_x= 0                # touche relachée 
                     elif event.key==pygame.K_LEFT:
                         self.joueur_vitesse_x= 0
-                    elif event.key == pygame.K_SPACE:
-                        self.player.playerChange()
-               
-               
 
             if self.sol.rect.colliderect(self.ennemi.rect):
                 self.ennemi.rect.y = 0
                 self.ennemi.color.couleur()
-
-            if self.player.rect.colliderect(self.ennemi.rect) :    # collision entre l'ennemi et le joueur 
-                if not self.ennemi.color.color == self.player.color :
-                    self.menu.run()
+                self.ennemi.rect.x = randint(50,450)
+                self.score.point += 1
+            if self.player.rect.colliderect(self.ennemi.rect):    # collision entre l'ennemi et le joueur 
+                self.menu.run()
 
 
             self.player.mouvement(self.joueur_vitesse_x)
             self.ennemi.mouvement(self.ennemi_vitesse_y)
             self.ecran.blit(self.image_fond,(0,0))
             #self.ecran.fill((255,255,255))
+
+            self.score.afficher()
 
             self.ennemi.afficher(self.ecran)
             self.sol.afficher(self.ecran)
@@ -78,3 +78,4 @@ class Jeu:
 
     def gravite_jeu(self):
         self.ennemi.rect.y += self.gravite[1] + self.resistance[1]
+

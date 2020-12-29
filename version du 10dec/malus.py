71a01e00a6733130f494e574b09043dd17377931
LeaDevaux <72825759+LeaDevaux@users.noreply.github.com>
2020-12-29 20:06:59 +0100

    Update malus.py

--- version du 10dec/malus.py
+++ version du 10dec/malus.py
@@ -9,29 +9,38 @@ pieuvre=pygame.transform.scale(pygame.image.load('images/pieuvre.png'),(30,30))
 
 class Malus(pygame.sprite.Sprite):
 
-    def __init__(self,ecran,x,y):
+    def __init__(self,ecran,h,v):
         super().__init__()
         self.listeMalus = [bomb, pieuvre]
-        self.x = x
-        self.y=y
+        self.x = h
+        self.y= v
         self.image=choice(self.listeMalus)
         self.rect=self.image.get_rect()
         self.ecran=ecran
 
         
     def mouvement(self,vitesse):
-        self.rect.y += vitesse
+        self.y += vitesse
+        self.rect.x=self.x
+        self.rect.y=self.y
 
     def afficher(self):
         self.rect.x=self.x
         self.rect.y=self.y
         self.ecran.blit(self.image,self.rect)
-
-    def effet(self):
-        #si c'est la pieuvre on affiche des cercles noirs pour cacher le jeu 
+        
+    def changement(self):
+        self.image= choice(self.listeMalus)
+        
+        
+    def effet(self,ecran):
+        #si c'est la pieuvre on affiche des cercles noirs pour cacher le jeu
+        noire=[0,0,0]
         if self.image == pieuvre:
             for i in (1,10):
-                pygame.draw.circle(ecran, (0,0,0),(randint(0,410),randint(0,410)),randint(5,20))
+                pygame.draw.circle(ecran, noire,(randint(0,410),randint(0,410)),randint(5,20))
+
+                
 
                 
 

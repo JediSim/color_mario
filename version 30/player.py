220c0698691690d3a0c2d7f0227bcc8b11121e23
LeaDevaux <72825759+LeaDevaux@users.noreply.github.com>
2021-01-08 00:01:34 +0100

    Add files via upload

--- version 30/player.py
+++ version 30/player.py
@@ -17,6 +17,7 @@ class Player:
 
         self.x = 240
         self.y = 400
+        self.vitesseX=5
         self.image = choice(self.listePlayers)
         self.rect=self.image.get_rect()
         self.ecran = ecran
@@ -31,7 +32,8 @@ class Player:
         
 
     def mouvement ( self, vitesse):
-        self.x+=vitesse
+        if self.x+vitesse<470 and self.x+vitesse>0 :
+            self.x+=vitesse
         self.rect.x=self.x
         self.rect.y=self.y
 
@@ -41,12 +43,20 @@ class Player:
         self.ecran.blit(self.image,self.rect)
 
     def playerChange(self):
-
+        imageprec=self.image
         self.image = choice(self.listePlayers)
-        
+        #permet de ne pas tomber sur la meme image
+        while imageprec==self.image:
+            self.image=choice(self.listePlayers)
+            
         if self.image==taod:
             self.color=(255,0,0)
         elif self.image==luigi:
             self.color=(0,255,0)
         else:
             self.color=(0,0,255)
+
+            
+    def vitesseChange(self):
+        self.vitesseX=3
+

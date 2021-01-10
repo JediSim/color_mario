f07f1033ec41846ce7ede886e3efdca72639e5d8
LeaDevaux <72825759+LeaDevaux@users.noreply.github.com>
2021-01-10 18:51:25 +0100

    Update bouton.py

--- Color_Mario/bouton.py
+++ Color_Mario/bouton.py
@@ -4,12 +4,12 @@ class Bouton:
     def __init__(self,abs,ord,long,large,ecran,texte,couleurTexte):
         self.rect = pygame.Rect(abs, ord, long, large)
         self.ecran = ecran
-
+        # gestion du texte et police ---------------------
         font=pygame.font.Font(None, 24)
         self.champs = font.render(texte,1,couleurTexte)
 
         corpsBouton = pygame.draw.rect(self.ecran,(0,0,0),self.rect)
         ecran.blit(self.champs,(abs+3,ord+3))
-
+        #gestion du clique --------------------------------
         self.mouse_pos = pygame.mouse.get_pos()
         self.isClique = corpsBouton.collidepoint(self.mouse_pos)
